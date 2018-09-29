c506b1f01d3e4bed5ebbe39e32d961a695278f00
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2018-09-29 17:30:38 +0530

    attempt edited versions of the word whenever insufficient results are found

--- fuzzy_autocomplete/core.py
+++ fuzzy_autocomplete/core.py
@@ -24,7 +24,7 @@ class Autocompleter:
         max_occurrence = max(self.words.values())
         substring_positions = collections.defaultdict(lambda: collections.defaultdict(set))
         for word in tqdm(self.words):
-            logger.info(f'Indexing {word}')
+            logger.debug(f'Indexing {word}')
             for substring, position in set(word_groups(word)):
                 substring_positions[substring][word].add(position)
         for substring, value in substring_positions.items():
@@ -38,20 +38,32 @@ class Autocompleter:
         print(f'Index size: {keys} keys, {values} values')
 
     def suggest_matches(self, word: str):
-        # TODO: Accurate ranking
         return itertools.islice(self._generate_fuzzy_matches(word), 25)
 
     def _generate_fuzzy_matches(self, word: str):
         # TODO: Fuzzy match. Tolerate typos
-        exclude = []
+        exclude = set()
+        for new_word in self._generate_matches(word, exclude):
+            yield new_word
+            exclude.add(new_word)
+        for edit in self._known_edits(word):
+            for new_word in self._generate_matches(edit, exclude):
+                yield new_word
+                exclude.add(new_word)
+
+    def _known_edits(self, word: str):
+        # Source: http://scottlobdell.me/2015/02/writing-autocomplete-engine-scratch-python/
+        length = len(word)
+        word_spits = [(word[:i], word[i:]) for i in range(1, length)]
+        for edit in [first + second[1:] for first, second in word_spits]:
+            if edit in self.word_index:
+                logger.debug(f'Known edit: {edit}')
+                yield edit
+
+    def _generate_matches(self, word: str, exclude: set):
         if word in self.words:
             yield word
-            exclude.append(word)
-        yield from self._generate_matches(word, exclude)
-
-    def _generate_matches(self, word: str, exclude: list):
-        # TODO: Improve performance
-        exclude = exclude[:]
+            exclude.add(word)
         try:
             bucket = self.word_index[word]
         except KeyError:
@@ -60,7 +72,7 @@ class Autocompleter:
             if new_word in exclude:
                 continue
             yield new_word
-            exclude.append(new_word)
+            exclude.add(new_word)
 
 
 filename = path.join(path.dirname(path.abspath(__file__)), 'word_search.tsv')
