43587c3627f6de741167566098a812a489a2fe7e
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2018-09-29 13:17:28 +0530

    adjust ranking criteria

--- fuzzy_autocomplete/core.py
+++ fuzzy_autocomplete/core.py
@@ -14,24 +14,32 @@ class Autocompleter:
     def __init__(self):
         with open(filename, 'r', newline='') as words_file:
             words_reader = csv.reader(words_file, delimiter='\t')
-            word_list = sorted(
-                ((word, int(occurrence)) for word, occurrence in words_reader),
-                key=operator.itemgetter(1), reverse=True)
-            self.words = dict(word_list)
-            self.word_index = collections.defaultdict(
-                lambda: collections.defaultdict(list))
-            for word in self.words:
-                for position, letter in enumerate(word):
-                    self.word_index[letter][word].append(position)
-            for key, value in self.word_index.items():
-                sorted_index = sorted(value.items(), key=lambda item: min(item[1]))
-                self.word_index[key] = {
-                    word: positions for word, positions in sorted_index}
+            self.words = {word: int(occurrence) for word, occurrence in words_reader}
+        self._index_words()
+
+    def _index_words(self):
+        self.word_index = {}
+        max_occurrence = max(self.words.values())
+        letter_positions = collections.defaultdict(lambda: collections.defaultdict(list))
+        for word in self.words:
+            for position, letter in enumerate(word):
+                letter_positions[letter][word].append(position)
+        for letter, value in letter_positions.items():
+            sorted_index = sorted(
+                ((word, (min(positions), max_occurrence - self.words[word], len(word)))
+                 for word, positions in value.items()),
+                key=operator.itemgetter(1))
+            self.word_index[letter] = [word for word, _ in sorted_index]
+        keys = len(self.word_index.keys())
+        values = sum(len(value) for value in self.word_index.values())
+        print(f'Index size: {keys} keys, {values} values')
 
     def suggest_matches(self, word: str):
+        # TODO: Accurate ranking
         return itertools.islice(self._generate_fuzzy_matches(word), 25)
 
     def _generate_fuzzy_matches(self, word: str):
+        # TODO: Fuzzy match. Tolerate typos
         exclude = []
         if word in self.words:
             logger.debug(f'Word: {word} Count: {self.words[word]}')
@@ -40,6 +48,7 @@ class Autocompleter:
         yield from self._generate_matches(word, exclude)
 
     def _generate_matches(self, word: str, exclude: list):
+        # TODO: Improve performance
         exclude = exclude[:]
         for letter in word:
             bucket = self.word_index[letter]
@@ -50,7 +59,7 @@ class Autocompleter:
                 exclude.append(new_word)
 
     def _generate_matches_in_bucket(
-            self, *, word: str, bucket: dict, exclude: list):
+            self, *, word: str, bucket: list, exclude: list):
         for dictionary_word in bucket:
             if dictionary_word in exclude:
                 continue
