54d47a442c230d58bc0ea9c2d86c0e4335032b04
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2018-09-29 12:35:27 +0530

    use an index for narrowing the search

--- fuzzy_autocomplete/core.py
+++ fuzzy_autocomplete/core.py
@@ -1,3 +1,4 @@
+import collections
 import csv
 import itertools
 import operator
@@ -17,6 +18,15 @@ class Autocompleter:
                 ((word, int(occurrence)) for word, occurrence in words_reader),
                 key=operator.itemgetter(1), reverse=True)
             self.words = dict(word_list)
+            self.word_index = collections.defaultdict(
+                lambda: collections.defaultdict(list))
+            for word in self.words:
+                for position, letter in enumerate(word):
+                    self.word_index[letter][word].append(position)
+            for key, value in self.word_index.items():
+                sorted_index = sorted(value.items(), key=lambda item: min(item[1]))
+                self.word_index[key] = {
+                    word: positions for word, positions in sorted_index}
 
     def suggest_matches(self, word: str):
         return itertools.islice(self._generate_fuzzy_matches(word), 25)
@@ -30,14 +40,25 @@ class Autocompleter:
         yield from self._generate_matches(word, exclude)
 
     def _generate_matches(self, word: str, exclude: list):
-        for dictionary_word, count in self.words.items():
+        exclude = exclude[:]
+        for letter in word:
+            bucket = self.word_index[letter]
+            suggestions = self._generate_matches_in_bucket(
+                word=word, bucket=bucket, exclude=exclude[:])
+            for new_word in suggestions:
+                yield new_word
+                exclude.append(new_word)
+
+    def _generate_matches_in_bucket(
+            self, *, word: str, bucket: dict, exclude: list):
+        for dictionary_word in bucket:
             if dictionary_word in exclude:
                 continue
             for letter in word:
                 if letter not in dictionary_word:
                     break
             else:
-                logger.debug(f'Word: {dictionary_word} Count: {count}')
+                logger.debug(f'Word: {dictionary_word} Count: {self.words[dictionary_word]}')
                 yield dictionary_word
 
 
